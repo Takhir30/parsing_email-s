b2fdf680954e332ddc940812dd04db7d2aeb47d6
Takhir <baygeriev87@mail.ru>
2018-06-16 03:09:51 +0300

    made some comments and separated some code in single function

--- Pars1.py
+++ Pars1.py
@@ -6,34 +6,36 @@ from urllib.request import urlopen
 from bs4 import BeautifulSoup
 
 
+# Getting the link to another page
 url = 'https://companies.dev.by'
 html = urlopen(url)
 bs = BeautifulSoup(html, 'html.parser')
 link = bs.find('tbody').tr.td.a
 
-
+# From new page getting the email
 new_url = url+link['href']
 new_html = urlopen(new_url)
 bs = BeautifulSoup(new_html, 'html.parser')
 email = bs.find('div', {'class': 'h-card'}).ul.li.a['href']
+send_email(email)
 
-print(email)
 
+# Mailing function
+def send_email(email):
+    fromaddr = "baygeriev87@mail.ru"
+    mypass = "password"
 
-fromaddr = "baygeriev87@mail.ru"
-mypass = "password"
+    msg = MIMEMultipart()
+    msg['From'] = fromaddr
+    msg['To'] = email
+    msg['Subject'] = "Привет"
 
-msg = MIMEMultipart()
-msg['From'] = fromaddr
-msg['To'] = email
-msg['Subject'] = "Привет"
+    body = "не обращай внимания"
+    msg.attach(MIMEText(body, 'plain'))
 
-body = "не обращай внимания"
-msg.attach(MIMEText(body, 'plain'))
+    server = smtplib.SMTP_SSL('smtp.mail.ru', 465)
 
-server = smtplib.SMTP_SSL('smtp.mail.ru', 465)
-
-server.login(fromaddr, mypass)
-text = msg.as_string()
-server.sendmail(fromaddr, toaddr, text)
-server.quit()
+    server.login(fromaddr, mypass)
+    text = msg.as_string()
+    server.sendmail(fromaddr, toaddr, text)
+    server.quit()
