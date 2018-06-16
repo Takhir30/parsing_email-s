3d137d43a54e934d84d12ac1d402d42bf106114e
Takhir <baygeriev87@mail.ru>
2018-06-16 22:43:08 +0300

    added my CV file to gitignore, and rebuild email function so it can deliver some files

--- Pars1.py
+++ Pars1.py
@@ -1,6 +1,7 @@
 import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
+from email.mime.application import MIMEApplication
 
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
@@ -24,19 +25,36 @@ for link in links:
 
 # Mailing function
 def send_email(email):
+    toaddr = email
     fromaddr = "baygeriev87@mail.ru"
     mypass = "password"
 
+    #email header
     msg = MIMEMultipart()
     msg['From'] = fromaddr
-    msg['To'] = email
-    msg['Subject'] = "Привет"
+    msg['To'] = toaddr
+    msg['Subject'] = "Ищу работу/Looking for job"
 
-    body = "не обращай внимания"
-    msg.attach(MIMEText(body, 'plain'))
+    #email body
+    body = """Здравствуйте!
+    Я начинающий Python Dev, который находится в поисках своей первой работы.
+    Я понимаю, что некоторые из вас не работают с начинающими программистами, а может просто
+    не используют Python, а может и то и другое. Дело в том, что мне было  ̶л̶е̶н̶ь̶  жаль времени,
+    чтобы получше узнать каждую компанию на dev.by. Проще и быстрее было распарсить сайт
+    и автоматом выслать всем вам письма с моим резюме. Надеюсь, что оно заинтересует кого нужно
+    и что не доставит больших неудобств кому не нужно.
+    Спасибо!"""
+    msg.attach(MIMEText(body))
+
+    #pdf attachment
+    filename = "MyCV.pdf"
+    fp = open(filename,'rb')
+    att = MIMEApplication(fp.read(),_subtype="pdf")
+    fp.close()
+    att.add_header('Content-Disposition','attachment',filename=filename)
+    msg.attach(att)
 
     server = smtplib.SMTP_SSL('smtp.mail.ru', 465)
-
     server.login(fromaddr, mypass)
     text = msg.as_string()
     server.sendmail(fromaddr, toaddr, text)
